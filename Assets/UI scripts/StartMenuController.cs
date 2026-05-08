fd4e0667cb1994bdd071e05c8fc202a07a838fd5
Cleo-Jo <c.jo@miniemen.school>
2026-05-08 12:05:40 +0200

    Back to main menu button

--- Assets/UI scripts/StartMenuController.cs
+++ Assets/UI scripts/StartMenuController.cs
@@ -6,7 +6,7 @@ public class StartMenuController : MonoBehaviour
 {
     public void OnStartClick()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("Cleo UI test scene"); // moet nog veranderen met defenitieve scene
     }
 
     public void OnExitClick()
