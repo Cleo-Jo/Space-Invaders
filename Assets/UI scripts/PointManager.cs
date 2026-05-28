e48a18b018a3bc6cd55d2fd723b7fc4a1f6fc868
Cleo-Jo <c.jo@miniemen.school>
2026-05-28 12:49:06 +0200

    Point manager script

--- Assets/UI scripts/PointManager.cs
+++ Assets/UI scripts/PointManager.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
+using TMPro;
 
 public class PointManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int score;
+    public TMP_Text ScoreText;
     void Start()
     {
-        
+       ScoreText.text = "SCoRE: " + score;
     }
 
     // Update is called once per frame
-    void Update()
+    
+    public void UpdateScore(int points)
     {
-        
+        score += points;
+        ScoreText.text = "SCoRE: " + score; 
     }
 }
