e355d98120b6ccd40a73667e1a9ff7e39cadeac2
Gael-Bck <g.braeckevelt@miniemen.school>
2026-05-29 10:37:02 +0200

    idk

--- Assets/backscenes/ShipMovement.cs
+++ Assets/backscenes/ShipMovement.cs
@@ -1,25 +1,73 @@
-using Unity.VisualScripting;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ShipMovement : MonoBehaviour
 {
+    private BoxCollider2D boxCollider;
+    private Transform visual;
 
     public float moveSpeed;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
-        
+        boxCollider = GetComponent<BoxCollider2D>();
+
+        // Visualizer setup
+        GameObject go = new GameObject("ColliderVisual");
+        go.transform.SetParent(transform);
+
+        SpriteRenderer sr = go.AddComponent<SpriteRenderer>();
+        sr.sprite = Resources.GetBuiltinResource<Sprite>("Sprites/Square.png");
+        sr.color = new Color(0f, 1f, 0f, 0.3f);
+
+        visual = go.transform;
+
+        UpdateColliderFromChildren();
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+
+        // Sync visual to collider
+        visual.localPosition = new Vector3(boxCollider.offset.x, boxCollider.offset.y, 0f);
+        visual.localScale = new Vector3(boxCollider.size.x, boxCollider.size.y, 1f);
+    }
+
+    void UpdateColliderFromChildren()
+    {
+        if (transform.childCount == 0) return;
+
+        float minX = float.MaxValue;  // ← was MinValue, should be MaxValue
+        float maxX = float.MinValue;
+
+        foreach (Transform child in transform)
+        {
+            // Skip the visual debug object itself
+            if (child.name == "ColliderVisual") continue;
+
+            float worldX = child.position.x;
+            if (worldX < minX) minX = worldX;
+            if (worldX > maxX) maxX = worldX;
+        }
+
+        float localMinX = transform.InverseTransformPoint(new Vector3(minX, 0, 0)).x;
+        float localMaxX = transform.InverseTransformPoint(new Vector3(maxX, 0, 0)).x;
+
+        float centerX = (localMinX + localMaxX) / 2f;
+        float width = localMaxX - localMinX;  // ← was min - max, should be max - min
+
+        Vector2 offset = boxCollider.offset;
+        offset.x = centerX;
+        boxCollider.offset = offset;
+
+        Vector2 size = boxCollider.size;
+        size.x = width;
+        boxCollider.size = size;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Boundery")
+        if (collision.gameObject.tag == "Boundery")
         {
             transform.position = new Vector3(transform.position.x, transform.position.y - 1, transform.position.z);
             moveSpeed *= -1;
